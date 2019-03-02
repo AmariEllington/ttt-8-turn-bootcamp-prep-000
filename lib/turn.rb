7b01a7a4c93ced3d3a6d2674b3c4115159c7314d
Amari Ellington <amari.l.ellington@gmail.com>
2019-03-02 15:29:48 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,3 +8,14 @@ puts " #{board[3]} #{separator} #{board[4]} #{separator} #{board[5]} "
 puts "#{lines}"
 puts " #{board[6]} #{separator} #{board[7]} #{separator} #{board[8]} "
 end
+
+# code your input_to_index and move method here!
+def input_to_index(user_input)
+ converted_input = user_input.to_i
+ converted_input = converted_input - 1
+end
+
+def move( board, index, character="X")
+  board[index]= character
+  return board
+end
